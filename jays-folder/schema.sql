56d21cb25f5d1509f69c141634bedeb503dc395e
Jay Sueno <jay.sueno@gmail.com>
2020-08-22 14:50:19 -0700

    finished jay and zhyi portion of the sql upload, waiting on johns csv

---
+++ jays-folder/schema.sql
@@ -0,0 +1,13 @@
+-- Create Two Tables
+CREATE TABLE fastfood (
+  city TEXT PRIMARY KEY,
+  number_fastfood TEXT
+);
+
+select * from fastfood
+
+CREATE TABLE expectancy (
+    city TEXT PRIMARY KEY,
+    life_expectancy FLOAT
+);
+
