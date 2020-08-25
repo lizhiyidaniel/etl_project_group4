1c9a2e6300f4caaaa9ab3d210c7072aad93a0306
Jay Sueno <jay.sueno@gmail.com>
2020-08-24 19:28:12 -0700

    finished jaysuenos 3 files

--- jays-folder/schema.sql
+++ jays-folder/schema.sql
@@ -1,12 +1,24 @@
--- Create Two Tables
+-- Schema for table creation in PostgreSQL
+-- Written by Jay Sueno
+
+-- Create Three Tables
 CREATE TABLE fastfood (
   city TEXT PRIMARY KEY,
   number_fastfood TEXT
 );
 
-select * from fastfood
-
 CREATE TABLE expectancy (
     city TEXT PRIMARY KEY,
     life_expectancy FLOAT
 );
+
+CREATE TABLE obesity (
+	city TEXT PRIMARY KEY,
+	percent_obese FLOAT
+);
+
+-- Query commands to check for proper creation of tables
+select * from fastfood
+select * from expectancy
+select * from obesity
+
